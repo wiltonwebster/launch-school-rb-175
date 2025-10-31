1662287ae65d8c124a78c2c6ccedf740180efb4d
Will Webster <wiltonwebster@gmail.com>
2025-10-31 21:01:59 +0000

    Isolating test execution

--- File_Based_CMS_Project/test/cms_test.rb
+++ File_Based_CMS_Project/test/cms_test.rb
@@ -2,35 +2,56 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require "fileutils"
 
 require_relative "../cms"
 
 class CMSTest < Minitest::Test
   include Rack::Test::Methods
-  
+
   def app
     Sinatra::Application
   end
+
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+  
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+  
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
   
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+    
     get "/"
     
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
   end
   
   def test_viewing_text_document
-    get "/history.txt"
+    create_document "changes.txt", "Here are some changes."
+    
+    get "/changes.txt"
     
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
-    assert_includes last_response.body, "1993 - Yukihiro Matsumoto dreams up Ruby."
+    assert_includes last_response.body, "Here are some changes."
   end
   
   def test_viewing_markdown_document
+    create_document "about.md", "# Ruby is..."
+    
     get "/about.md"
     
     assert_equal 200, last_response.status
@@ -54,6 +75,8 @@ class CMSTest < Minitest::Test
   end
   
   def test_editing_document
+    create_document "changes.txt"
+    
     get "/changes.txt/edit"
     
     assert_equal 200, last_response.status
@@ -68,7 +91,6 @@ class CMSTest < Minitest::Test
     
     get last_response["Location"]
     
-    assert_equal 200, last_response.status
     assert_includes last_response.body, "changes.txt has been updated."
     
     get "/changes.txt"
