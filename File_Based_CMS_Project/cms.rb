122dec9342e37622245870635287798202de9a7c
Will Webster <wiltonwebster@gmail.com>
2025-11-05 23:08:16 +0000

    Storing user accounts in an external file

--- File_Based_CMS_Project/cms.rb
+++ File_Based_CMS_Project/cms.rb
@@ -4,6 +4,7 @@ require "sinatra/content_for"
 require "tilt"
 require "erubis"
 require "redcarpet"
+require "yaml"
 
 configure do
   enable :sessions
@@ -16,6 +17,15 @@ def data_path
   else
     File.expand_path("../data", __FILE__)
   end
+end
+
+def load_user_credentials
+  credentials_path = if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/users.yml", __FILE__)
+  else
+    File.expand_path("../users.yml", __FILE__)
+  end
+  YAML.load_file(credentials_path)
 end
 
 def render_markdown(text)
@@ -59,8 +69,11 @@ get "/users/signin" do
 end
 
 post "/users/signin" do
-  if params[:username] == "admin" && params[:password] == "secret"
-    session[:username] = params[:username]
+  credentials = load_user_credentials
+  username = params[:username]
+  
+  if credentials.key?(username) && credentials[username] == params[:password]
+    session[:username] = username
     session[:message] = "Welcome!"
     redirect "/"
   else
